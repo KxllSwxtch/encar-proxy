2d376a429a82910b7a60a45172e590264961c164
KxllSwxtch <dimashin9@gmail.com>
2025-04-14 08:39:12 +0900

    updates

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Query
 import httpx
+import random
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,11 +14,38 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# @app.get("/api/catalog")
+# async def proxy_catalog(q: str = Query(...), sr: str = Query(...)):
+#     url = f"https://api-encar.habsidev.com/api/catalog?count=true&q={q}&sr={sr}"
+
+#     async with httpx.AsyncClient() as client:
+#         response = await client.get(url)
+
+#     return response.json()
+
+
 @app.get("/api/catalog")
 async def proxy_catalog(q: str = Query(...), sr: str = Query(...)):
-    url = f"https://api-encar.habsidev.com/api/catalog?count=true&q={q}&sr={sr}"
+    url = f"https://api.encar.com/search/car/list/general?count=true&q={q}&sr={sr}"
+
+    headers = {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "en,ru;q=0.9,en-CA;q=0.8,la;q=0.7,fr;q=0.6,ko;q=0.5",
+        "Origin": "http://www.encar.com",
+        "Referer": "http://www.encar.com/",
+        "User-Agent": random.choice(
+            [
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+            ]
+        ),
+    }
 
     async with httpx.AsyncClient() as client:
-        response = await client.get(url)
+        response = await client.get(url, headers=headers)
 
     return response.json()
