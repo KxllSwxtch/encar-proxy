d3d0aa3bafa56a0ae60635c63368e1824ea20df4
Dmitriy Shin <dimashin9@gmail.com>
2025-06-06 08:42:53 +0900

    updaets

--- curlrequest.py
+++ curlrequest.py
@@ -1,8 +1,18 @@
 import requests
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
-    "Referer": "https://www.intercarkorea.com/",
+    "accept": "*/*",
+    "accept-language": "en,ru;q=0.9,en-CA;q=0.8,la;q=0.7,fr;q=0.6,ko;q=0.5",
+    "origin": "https://www.intercarkorea.com",
+    "priority": "u=1, i",
+    "referer": "https://www.intercarkorea.com/",
+    "sec-ch-ua": '"Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
 }
 
 response = requests.get(
